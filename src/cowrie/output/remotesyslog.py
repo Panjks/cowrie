2009e7e6cac67b51b2cce2b314a2114cbce13b2c
panjks- <panjks@163.com>
2018-12-06 17:53:38 +0800

    add post_file module in remotesyslog

--- src/cowrie/output/remotesyslog.py
+++ src/cowrie/output/remotesyslog.py
@@ -12,6 +12,12 @@ from __future__ import absolute_import, division
 import logging
 import logging.handlers
 import json
+import os
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
+import requests
 
 import cowrie.core.output
 from cowrie.core.config import CONFIG
@@ -31,6 +37,8 @@ class Output(cowrie.core.output.Output):
         fh.setFormatter(formatter)
         self.logger.addHandler(fh)
 
+
+
     def stop(self):
         pass
 
@@ -41,3 +49,41 @@ class Output(cowrie.core.output.Output):
                 del logentry[i]
 
         self.logger.error(json.dumps(logentry).rstrip('\t\r\n\0'))
+
+        if logentry["eventid"] == "cowrie.session.file_download":
+            print("Sending file to Server")
+            p = urlparse(logentry["url"]).path
+            if p == "":
+                fileName = logentry["shasum"]
+            else:
+                b = os.path.basename(p)
+                if b == "":
+                    fileName = logentry["shasum"]
+                else:
+                    fileName = b
+
+            self.postfile(logentry["outfile"], fileName)
+
+        elif logentry["eventid"] == "cowrie.session.file_upload":
+            print("Sending file to Server")
+            self.postfile(logentry["outfile"], logentry["filename"])
+
+
+    def postfile(self, artifact, fileName):
+        """
+        Send a file to Private server
+        """
+        if self.enabled:
+            try:
+                res = requests.post(
+                    "https://122.228.19.76:9999/upload.php",
+                    files={fileName: open(artifact, "rb")},
+                    verify=False
+                )
+                if res and res.ok:
+                    print("Submited to Server")
+                else:
+                    print("Server Request failed: {}".format(res.status_code))
+            except Exception as e:
+                print("Server Request failed: {}".format(e))
+        return
